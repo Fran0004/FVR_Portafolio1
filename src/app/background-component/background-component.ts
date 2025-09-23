16dc461a05b0fd9c8aa6f3eee0c4873ed67fdad1
Fran0004 <116067683+Fran0004@users.noreply.github.com>
2025-09-23 14:16:53 +0200

    bubbles aleatorias

--- src/app/background-component/background-component.ts
+++ src/app/background-component/background-component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-background-component',
@@ -6,6 +7,45 @@ import { Component } from '@angular/core';
   templateUrl: './background-component.html',
   styleUrl: './background-component.scss'
 })
-export class BackgroundComponent {
+export class BackgroundComponent implements OnInit, OnDestroy {
+    bubbles: {
+    id: number;
+    size: number;
+    left: number;
+    duration: number;
+    opacity: number;
+    blur : number;
 
+  }[] = [];
+
+  private intervalId: any;
+  private bubbleCounter = 0;
+
+  ngOnInit(): void {
+      this.intervalId = setInterval(() =>{if(this.bubbles.length < 20 ){
+        this.addBubble()
+      }} , 500);
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId);
+  }
+
+  addBubble(): void {
+    const bubble = {
+      id: this.bubbleCounter++,
+      size: Math.random() * 30 + 10,        // 10px - 40px
+      left: Math.random() * 100,           // porcentaje
+      duration: Math.random() * 10 + 10,    // 5s - 15s
+      opacity: Math.random() * 0.3 + 0.05,  // 0.05 - 0.35
+      blur: Math.random() * 3 + 10
+    };
+    console.log(this.bubbles.length)
+    this.bubbles.push(bubble);
+
+    // eliminar después de la animación
+    setTimeout(() => {
+      this.bubbles = this.bubbles.filter(b => b.id !== bubble.id);
+    }, (bubble.duration ) * 1000);
+  }
 }
