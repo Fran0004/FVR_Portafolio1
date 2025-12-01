d44fb4a8ff8a367d5faadfdfcf3cc0255f285753
Fran0004 <116067683+Fran0004@users.noreply.github.com>
2025-12-01 22:17:40 +0100

    updates to responsive and colors

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,4 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { ContactComponent } from './contact-component/contact-component';
 import { ExpComponent } from './exp-component/exp-component';
 import { NameComponent } from "./name-component/name-component";
